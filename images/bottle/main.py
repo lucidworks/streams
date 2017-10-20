7bf35bd4848ff031e4cec0377432196f40af6815
Kord Campbell <kordless@gmail.com>
2017-10-20 16:00:56 -0700

    episode 2 dump

--- images/bottle/main.py
+++ images/bottle/main.py
@@ -1,11 +1,15 @@
-from bottle import route, run, template
+from bottle import route, run, template, static_file
 
-@route('/api/')
-def api():
+@route('/api/<method>')
+def api(method):
     from bottle import response
     from json import dumps
     response.content_type = 'application/json'
-    res = {'x': 1}
+    res = {'x': 1, 'method': method}
     return template(dumps(res))
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='/static')
 
 run(host='0.0.0.0', port=8081)
