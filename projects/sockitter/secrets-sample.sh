e804144e1494eb16ce760aa0dd73e37fef88dfa3
Kord Campbell <kordless@gmail.com>
2018-04-27 12:43:23 -0700

    new download file

---
+++ projects/sockitter/secrets-sample.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# set these to your twitter credentials and rename this to secrets.sh before starting instance
+TOKEN=#####################
+TOKEN_SECRET=#####################
+CONSUMER_KEY=#####################
+CONSUMER_SECRET=#####################
+
+export TOKEN
+export TOKEN_SECRET
+export CONSUMER_KEY
+export CONSUMER_SECRET
