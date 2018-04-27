e02cc161da83d6d6f6d73fd566e9e60c97a75471
Kord Campbell <kordless@gmail.com>
2018-04-27 13:48:42 -0700

    template for password/tokens

--- projects/sockitter/start-sockitter.sh
+++ projects/sockitter/start-sockitter.sh
@@ -11,6 +11,42 @@ fi
 
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n 1)
 
+SCRIPT='#! /bin/bash
+sudo su -
+apt-get update -y
+sudo add-apt-repository ppa:webupd8team/java -y
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get update -y
+
+sudo apt-get install oracle-java8-installer -y
+sudo apt install oracle-java8-set-default -y
+
+echo JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> /etc/environment
+
+apt-get install unzip -y
+apt-get install ant -y
+
+cd /; git clone https://github.com/lucidworks/streams
+sed -i "
+s,%YOUR_TOKEN%,$TOKEN,g;
+s,%YOUR_TOKEN_SECRET%,$TOKEN_SECRET,g;
+s,%YOUR_CONSUMER_KEY%,$CONSUMER_KEY,g;
+s,%YOUR_CONSUMER_SECRET%,$CONSUMER_SECRET,g;
+" /streams/projects/sockitter/dev/app.properties
+
+# only download and untar if we do not have a /fusion directory
+if [ ! -d "/fusion" ]; then
+wget https://storage.googleapis.com/streams-fusion/fusion-4.0.1.tar.gz
+tar xvfz fusion-4.0.1.tar.gz
+fi
+
+cd /
+/fusion/4.0.1/bin/fusion restart
+'
+
+echo $SCRIPT
+
 gcloud compute instances create fusion-sockitter-$NEW_UUID \
 --machine-type "n1-standard-8" \
 --image "ubuntu-1604-xenial-v20180405" \
@@ -22,35 +58,7 @@ gcloud compute instances create fusion-sockitter-$NEW_UUID \
 --labels ready=true \
 --tags lucid \
 --preemptible \
---metadata startup-script='#! /bin/bash
-sudo su -
-apt-get update -y
-sudo add-apt-repository ppa:webupd8team/java -y
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-apt-get update -y
-
-IP=$(curl ipecho.net/plain)
-
-sudo apt-get install oracle-java8-installer -y
-sudo apt install oracle-java8-set-default -y
-
-echo JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> /etc/environment
-
-apt-get install unzip -y
-apt-get install ant -y
-
-cd /; git clone https://github.com/lucidworks/streams
-
-# only download and untar if we do not have a /fusion directory
-if [ ! -d "/fusion" ]; then
-wget https://storage.googleapis.com/streams-fusion/fusion-4.0.1.tar.gz
-tar xvfz fusion-4.0.1.tar.gz
-fi
-
-cd /
-/fusion/4.0.1/bin/fusion restart
-'
+--metadata startup-script=$SCRIPT
 
 sleep 15
 
