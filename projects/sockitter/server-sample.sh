0b66732cf7afcf25e82589c12d0967e71dae726d
Kord Campbell <kordless@gmail.com>
2018-04-27 15:35:08 -0700

    access token

--- projects/sockitter/server-sample.sh
+++ projects/sockitter/server-sample.sh
@@ -19,7 +19,7 @@ IP=$(wget -qO- http://ipecho.net/plain)
 cd /; git clone https://github.com/lucidworks/streams
 
 sed -i "
-s,YOUR_TOKEN,%TOKEN%,g;
+s,YOUR_ACESS_TOKEN,%ACESS_TOKEN%,g;
 s,YOUR_TOKEN_SECRET,%TOKEN_SECRET%,g;
 s,YOUR_CONSUMER_KEY,%CONSUMER_KEY%,g;
 s,YOUR_CONSUMER_SECRET,%CONSUMER_SECRET%,g;
