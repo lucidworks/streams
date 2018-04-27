a25d117dfc5365ddba95a797116a3145571210a2
Kord Campbell <kordless@gmail.com>
2018-04-27 15:20:56 -0700

    secret token token secret

--- projects/sockitter/secrets-sample.sh
+++ projects/sockitter/secrets-sample.sh
@@ -2,14 +2,8 @@
 
 # set these to your twitter credentials and rename this to secrets.sh before starting instance
 TOKEN=********
-TOKEN_SECRET=********
+SECRET_TOKEN=********
 CONSUMER_KEY=********
 CONSUMER_SECRET=********
 FUSION_PASSWORD=********
-
-export TOKEN
-export TOKEN_SECRET
-export CONSUMER_KEY
-export CONSUMER_SECRET
-export FUSION_PASSWOR
 
