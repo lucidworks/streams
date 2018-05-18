ec056be2ef2c29cbee3f834dc88764150e3dc1c0
Kord Campbell <kordless@gmail.com>
2018-05-17 22:03:21 -0700

    skg deploy

--- projects/sockitter/server-sample.sh
+++ projects/sockitter/server-sample.sh
@@ -12,6 +12,7 @@ sudo apt install oracle-java8-set-default -y
 echo JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> /etc/environment
 
 apt-get install unzip -y
+apt-get install maven -y
 apt-get install ant -y
 
 IP=$(wget -qO- http://ipecho.net/plain)
@@ -36,6 +37,25 @@ wget https://storage.googleapis.com/streams-fusion/fusion-4.0.1.tar.gz
 tar xvfz fusion-4.0.1.tar.gz
 fi
 
+# link up fusion
 ln -s /fusion/ /root/fusion
-cd /
+
+# build SKG FTW
+mkdir /skg; cd /skg
+git clone https://github.com/treygrainger/semantic-knowledge-graph.git
+cd semantic-knowledge-graph
+git checkout solr_7.2.1
+cd knowledge-graph
+
+# restart
 /fusion/4.0.1/bin/fusion restart
+
+# curling binaries
+curl -X POST -H 'Content-Type: application/octet-stream' --data-binary @semantic-knowledge-graph-1.0-SNAPSHOT
+.jar http://localhost:8983/solr/.system/blob/skg.jar
+curl http://localhost:8983/solr/sockitter/config -H 'Content-type:application/json' -d '{  "update-runtimelib ": { "name":"skg.jar", "version":1 } }'
+curl http://localhost:8983/solr/sockitter/config -H 'Content-type:application/json' -d '{  "add-runtimelib": { "name":"skg.jar", "version":1 } }'
+curl http://localhost:8983/solr/sockitter/config -H 'Content-type:application/json' -d '{  "add-queryresponsewriter": { "name": "skg",    "runtimeLib": true,    "class": "com.careerbuilder.search.relevancy.responsewriter.KnowledgeGraphResponseWriter"} }'
+curl http://localhost:8983/solr/sockitter/config -H 'Content-type:application/json' -d '{  "add-requesthandler" : {  "name": "/skg",    "class":"com.careerbuilder.search.relevancy.KnowledgeGraphHandler",    "defaults":{ "defType":"edismax", "wt":"json"},    "invariants":{"wt":"skg"},    "runtimeLib": true  } }'
+curl http://localhost:8983/solr/sockitter/config -H 'Content-type:application/json' -d '{  "create-runtimelib": { "name":"skg.jar", "version":1 } }'
+curl http://localhost:8983/solr/sockitter/config -H 'Content-type:application/json' -d '{  "update-requesthandler" : {  "name": "/skg",    "class":"com.careerbuilder.search.relevancy.KnowledgeGraphHandler",    "defaults":{ "defType":"edismax", "wt":"json"},    "invariants":{"wt":"skg"},    "runtimeLib": true  } }'
