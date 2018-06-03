c730ba2ae7fc670bc738faba8b2d4b9a43edbeab
Erik Hatcher <erik.hatcher@lucidworks.com>
2018-06-03 12:10:29 -0400

    overhaul of webapp API and editor usage

--- projects/sockitter/dev/src/com/lucidworks/streams/sockitter/FusionGateway.java
+++ projects/sockitter/dev/src/com/lucidworks/streams/sockitter/FusionGateway.java
@@ -27,63 +27,108 @@ import java.util.*;
 /**
  * Fusion's twitter stream data source is configured with Twitter `id`s.  This servlet provides endpoints
  * to add Twitter accounts to follow by screen name rather than `id`.
- *
+ * <p>
  * This service encapsulates the Fusion connector API along with use of the 3rd party Twitter4j API to look up `id`s
  * by screen names, de-duplicating the list.
- *
+ * <p>
  * Examples:
- *   lookupUsersByIDs: http://localhost:8780/sockitter-editor/lookupUsersByIDs?id=1138371924
- *   lookupUsersByScreenNames: http://localhost:8780/sockitter-editor/lookupUsersByScreenNames?screen_name=erikhatcher
- *   add: http://localhost:8780/sockitter-editor/add?ds_name=tweets&screen_name=erikhatcher
+ * lookupUsersByIDs: http://localhost:8780/sockitter-editor/lookupUsersByIDs?id=1138371924
+ * lookupUsersByScreenNames: http://localhost:8780/sockitter-editor/lookupUsersByScreenNames?screen_name=erikhatcher
+ * add: http://localhost:8780/sockitter-editor/add?ds_name=tweets&screen_name=erikhatcher
  */
 public class FusionGateway extends HttpServlet {
   static {
     // without this, NoClassDefFound error (due to slf4j not on classpath?)
-    System.setProperty("twitter4j.loggerFactory","twitter4j.NullLoggerFactory");
+    System.setProperty("twitter4j.loggerFactory", "twitter4j.NullLoggerFactory");
   }
 
   @Override
   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     response.setContentType("application/json");
+    String method = request.getMethod();
 
     Map data = new HashMap();
-    data.put("uri",request.getRequestURI());
+    data.put("uri", request.getRequestURI());
     data.put("context_path", request.getContextPath());
     data.put("servlet_path", request.getServletPath());
-    data.put("method", request.getMethod());
+    data.put("method", method);
 
-    String path = request.getServletPath();
+    int endpoint_pos = request.getContextPath().length() + request.getServletPath().length();
+    String endpoint = request.getRequestURI().substring(endpoint_pos);
+    data.put("endpoint", endpoint);
 
-    // TODO: implement "clear" and "remove" type actions, perhaps?
 
-    switch (path) {
-      case "/add":
+    switch (endpoint) {
+      case "/config":
+        if (!"POST".equals(method)) {
+          throw new ServletException("Method " + method + " not supoorted at this endpoint.  Must use POST");
+        }
+
         String ds_name = request.getParameter("ds_name");
         if (ds_name == null) {
           throw new ServletException("ds_name must be provided");
         }
-        String[] screen_names = request.getParameterValues("screen_name");
-        addScreenNames(ds_name, screen_names);
-        data.put("screen_names", screen_names);
+
+        JsonObject ds = loadDatasource(ds_name);
+
+        // TODO:
+        //   - update JSON blob with creds    (or application scope?)
+        //   - update datasource with creds
+        //   - update index pipeline with google vision key
+        break;
+
+      case "/follow":
+        ds_name = request.getParameter("ds_name");
+        if (ds_name == null) {
+          throw new ServletException("ds_name must be provided");
+        }
+
+        if ("POST".equals(method)) {
+          String[] screen_names = request.getParameterValues("screen_name");
+          updateScreenNames(ds_name, screen_names, false);
+          data.put("screen_names", screen_names);
+        } else {
+          ds = loadDatasource(ds_name);
+          JsonObject properties = ds.getAsJsonObject("properties");
+          JsonArray filter_follow = properties.getAsJsonArray("filter_follow");
+
+          long[] ids = new long[filter_follow.size()];
+          for (int i = 0; i < filter_follow.size(); i++) {
+            ids[i] = Long.parseLong(filter_follow.get(i).getAsString());
+          }
+
+          if (ids.length > 0) {
+            ResponseList<User> users = lookupUsersByIDs(ids);
+            data.put("response", users);
+
+            String[] screen_names = new String[users.size()];
+            for (int i = 0; i < users.size(); i++) {
+              screen_names[i] = users.get(i).getScreenName();
+            }
+            data.put("screen_names", screen_names);
+          } else {
+            data.put("message", "No ids being followed");
+          }
+        }
         break;
 
       case "/lookupUsersByScreenNames":
-        screen_names = request.getParameterValues("screen_name");
+        String[] screen_names = request.getParameterValues("screen_name");
         data.put("response", lookupUsersByScreenNames(screen_names));
         break;
 
       case "/lookupUsersByIDs":
         String[] str_ids = request.getParameterValues("id");
         long[] ids = new long[str_ids.length];
-        for (int i=0; i < str_ids.length; i++) {
+        for (int i = 0; i < str_ids.length; i++) {
           ids[i] = Long.parseLong(str_ids[i]);
         }
         data.put("response", lookupUsersByIDs(ids));
         break;
 
       default:
-        data.put("error","Path " + path + " not supported");
+        data.put("error", "Path " + endpoint + " not supported");
         break;
     }
 
@@ -92,7 +137,7 @@ public class FusionGateway extends HttpServlet {
     gson.toJson(data, out);
   }
 
-  private static void addScreenNames(String ds_name, String[] screen_names) throws IOException {
+  private static void updateScreenNames(String ds_name, String[] screen_names, boolean append) throws IOException {
     ResponseList<User> users = lookupUsersByScreenNames(screen_names);
     JsonObject ds = loadDatasource(ds_name);
 
@@ -101,8 +146,11 @@ public class FusionGateway extends HttpServlet {
 
     // dedupe and replace the id list; probably cleaner ways to do this
     Set<String> following = new HashSet<String>();
-    for (JsonElement f : filter_follow) {
-      following.add(f.getAsString());
+
+    if (append) { // include existing ones
+      for (JsonElement f : filter_follow) {
+        following.add(f.getAsString());
+      }
     }
     for (User user : users) {
       following.add("" + user.getId());
@@ -216,12 +264,12 @@ public class FusionGateway extends HttpServlet {
 //      e.printStackTrace();
 //    }
 
-      try {
-        // 1138371924
-        System.out.println(lookupUsersByIDs(new long[] {1138371924}));
-        // addScreenNames("tweets", new String[]{"stackgeek","erikhatcher", "kordless"});
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+    try {
+      // 1138371924
+      System.out.println(lookupUsersByIDs(new long[]{1138371924}));
+      // addScreenNames("tweets", new String[]{"stackgeek","erikhatcher", "kordless"});
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
