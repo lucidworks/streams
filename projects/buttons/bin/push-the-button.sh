1833efa29ed1ddc8f3fd8df6f14fcefd664b8775
Erik Hatcher <erik.hatcher@lucidworks.com>
2018-07-14 15:08:51 -0400

    make SID dynamic from

--- projects/buttons/bin/push-the-button.sh
+++ projects/buttons/bin/push-the-button.sh
@@ -4,26 +4,16 @@
 
 # World, the time has come to...
 
-option="$1"
-PREEMPTIBLE="--preemptible"
-case $option in
-    -p|--prod|--production)
-    unset PREEMPTIBLE
-esac
-
-if [ -n "$PREEMPTIBLE" ]; then
-   echo "If you need a permanent version of this server, run the following:"
-   echo "./bin/push-the-button.sh --production"
-else
-   echo "Starting a production version of button $SID..."
+if [ -z "$1" ]; then
+  echo "What button do you want to push?   'push-the-button.sh <button ID>'" >&2
+  exit 1
 fi
 
+SID="$1"
+
+# TODO: should we check that IID doesn't already exist and loop until we get a new one?
 IID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 4 | head -n 1)
 ZONE=us-west1-c
-
-# TODO: SID will come from launcher
-SID=rules
-# SID=lou
 
 gcloud compute instances create button-$SID-$IID \
 --machine-type "n1-standard-4" \
@@ -47,5 +37,5 @@ gcloud compute firewall-rules create fusion-appkit --allow tcp:8080
 gcloud compute firewall-rules create fusion-webapp --allow tcp:8780
 gcloud compute firewall-rules create fusion-webapp --allow tcp:9999
 
-echo "Button $SID pressed and launched:"
+echo "Button $SID pushed and launched"
 echo "Fusion UI available in a few minutes at: http://$IP:8764"
