eec10a561a0a1b3e2184285a189fc0abea1da75a
Kord Campbell <kordless@gmail.com>
2018-07-17 14:15:16 -0700

    move tag

--- projects/buttons/fastener/start-fastener.sh
+++ projects/buttons/fastener/start-fastener.sh
@@ -30,13 +30,12 @@ gcloud beta compute instances create $NAME-$NEW_UUID \
 --boot-disk-type "pd-ssd" \
 --boot-disk-device-name "$NAME-disk-$NEW_UUID" \
 --zone $ZONE \
---labels token=$TOKEN \
---tags lucid \
+--tags lucid token=$TOKEN \
 --scopes compute-rw \
 --subnet=default --address=35.230.26.45 --network-tier=PREMIUM \
 --service-account labs-209320@appspot.gserviceaccount.com \
 $PREEMPTIBLE \
---metadata token=$TOKEN,startup-script='#! /bin/bash
+--metadata startup-script='#! /bin/bash
 sudo su -
 
 apt-get update -y
