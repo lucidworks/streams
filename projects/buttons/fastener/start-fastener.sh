c30f17b758b145501177f32795d2822ec7345481
Kord Campbell <kordless@gmail.com>
2018-07-16 21:01:55 -0700

    different service account?

--- projects/buttons/fastener/start-fastener.sh
+++ projects/buttons/fastener/start-fastener.sh
@@ -22,7 +22,7 @@ gcloud compute instances create $NAME-$NEW_UUID \
 --labels ready=true \
 --tags lucid \
 --scopes compute-rw \
---service-account 473791411459-compute@developer.gserviceaccount.com \
+--service-account kord.campbell@lucidworks.com \
 $PROD \
 --metadata startup-script='#! /bin/bash
 sudo su -
