9d154e11f08894fe25bf6ef6445e8b2a53ee549e
Kord Campbell <kordless@gmail.com>
2018-07-19 09:58:16 -0700

    http port

--- projects/buttons/fastener/start-fastener.sh
+++ projects/buttons/fastener/start-fastener.sh
@@ -32,7 +32,7 @@ gcloud beta compute instances create $NAME-$NEW_UUID \
 --boot-disk-type "pd-ssd" \
 --boot-disk-device-name "$NAME-disk-$NEW_UUID" \
 --zone $ZONE \
---tags lucid,token-$TOKEN \
+--tags http-server,lucid,token-$TOKEN \
 --scopes compute-rw \
 --subnet=default --address=35.230.26.45 --network-tier=PREMIUM \
 --service-account labs-209320@appspot.gserviceaccount.com \
