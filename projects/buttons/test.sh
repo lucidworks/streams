3441b7cfcc8a38ca42f6fdf8c7575b2df8f867a5
Kord Campbell <kordless@gmail.com>
2018-12-21 22:16:50 -0800

    hopefully fix instance start issue

--- projects/buttons/test.sh
+++ projects/buttons/test.sh
@@ -2,4 +2,5 @@
 # curl https://streams.lucidworks.com/api/instance/button-activate-2tih3b?token=6fb7DSGbD3yR6HetcNfV7blQUQi6LZ9zIDiJx40NdTWFQPRBnF7UnX5HuJ99c8LF
 # curl https://streams.lucidworks.com/api/stream/starter41?token=6fb7DSGbD3yR6HetcNfV7blQUQi6LZ9zIDiJx40NdTWFQPRBnF7UnX5HuJ99c8LF
 # curl -d 'Content-length: 0' -X POST https://streams.lucidworks.com/api/stream/starter41?token=6fb7DSGbD3yR6HetcNfV7blQUQi6LZ9zIDiJx40NdTWFQPRBnF7UnX5HuJ99c8LF
-curl https://streams.lucidworks.com/api/instance/button-finance360-x4be0c/start?token=6fb7DSGbD3yR6HetcNfV7blQUQi6LZ9zIDiJx40NdTWFQPRBnF7UnX5HuJ99c8LF
+# curl https://streams.lucidworks.com/api/instance/button-finance360-x4be0c/start?token=6fb7DSGbD3yR6HetcNfV7blQUQi6LZ9zIDiJx40NdTWFQPRBnF7UnX5HuJ99c8LF
+curl https://streams.lucidworks.com/api/status?token=6fb7DSGbD3yR6HetcNfV7blQUQi6LZ9zIDiJx40NdTWFQPRBnF7UnX5HuJ99c8LF
