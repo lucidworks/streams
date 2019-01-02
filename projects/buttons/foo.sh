b52b4a0e1cf2af750d17d0459d152453fd207cd0
Kord Campbell <kordless@gmail.com>
2019-01-01 17:47:17 -0800

    remove testing file

--- projects/buttons/foo.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST \
--d 'Content-length: 0' \
-"https://streams.lucidworks.com/api/stream/starter41?token=04ALAnF45N5P6fwpkqRvwb4TBiboRDrsVa6WOO6vsyJ9EzvjSGeIF7XJASRHUQTT&preemtible=0&topic=space"
-
