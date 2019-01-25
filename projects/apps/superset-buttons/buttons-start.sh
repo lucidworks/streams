f506eea6f2cd24e7d34ed1586d328f3f67cc6974
Erik Hatcher <erik.hatcher@lucidworks.com>
2019-01-25 14:55:17 -0500

    superskeleton

--- projects/apps/superset-buttons/buttons-start.sh
+++ projects/apps/superset-buttons/buttons-start.sh
@@ -1,3 +1,28 @@
 set -x
 
-curl -u $FUSION_API_CREDENTIALS -H "Content-Type:multipart/form-data" -X POST -F 'importData=@Fusion_Starter_for_4_1_0.zip' $FUSION_API_BASE/objects/import?importPolicy=overwrite
+# Steps:
+#   Download and install Apache Superset
+#     - TODO: need to included Superset distro here
+
+#   Download and install Lucidworks Fusion
+#     - done (Fusion already assumed running at this point in the script)
+
+#   Configure Lucidworks Fusion to work in `binary` mode.
+#     - TODO: config file tinkering
+
+#   Spin up Apache Superset and Lucidworks Fusion.
+#     - TODO: start Superset
+#     - TODO: restart Fusion?
+
+#   Create an app in Lucidworks Fusion and index data so that you have at least one collection.
+#     - TODO: include app here, that has a blob datasource
+#     - TODO: start the datasource here?
+
+#   Connect to Lucidworks Fusion to Superset in the Superset UI and add tables from your Fusion collection into Superset.
+#     - TODO: can we bake this into some pre-installed Superset config?
+
+#   Create Your First Chart
+#     - TODO: can we bake this in?
+
+# Diagnostics
+python --version
