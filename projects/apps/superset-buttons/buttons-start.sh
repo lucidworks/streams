27e0df0d0be4a6fd1bd1507f565aaa0700c91130
Erik Hatcher <erik.hatcher@lucidworks.com>
2019-01-27 08:49:09 -0500

    first pass at integrating python and superset commands

--- projects/apps/superset-buttons/buttons-start.sh
+++ projects/apps/superset-buttons/buttons-start.sh
@@ -73,3 +73,69 @@ FLASK_ENV=development flask run -p 8088 --with-threads --reload --debugger
 
 # Diagnostics
 python --version
+
+diff -r fusion_conf/conf /fusion/conf
+
+set -x
+
+# Steps:
+#   Download and install Apache Superset
+#     - TODO: need to included Superset distro here
+
+#install core utilities
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev python-pip libsasl2-dev libldap2-dev
+
+#install virtualenv
+sudo apt install python3-venv
+
+#name that virtual environment
+pyvenv my_venv
+
+#activate the virtual environment
+source my_env/bin/activate
+
+python --version
+
+
+#install setuptools in the virtual env
+pip install --upgrade setuptools pip
+
+# pull the superset master from github.
+git clone https://github.com/apache/incubator-superset.git
+
+#pull in dependencies
+pip install -r requirements.txt
+
+#pull in dependencies
+pip install -r requirements-dev.txt
+
+#editable mode
+
+pip install -e .
+pip install python-dotenv
+
+# create a user
+fabmanager create-admin --app superset
+
+# Initialize the database
+superset db upgrade
+
+# Load some data to play with
+superset load_examples
+
+# Create default roles and permissions
+superset init
+
+# To start a development web server on port 8088, use -p to bind to another port
+
+
+#   Download and install Lucidworks Fusion
+#     - done (Fusion already assumed running at this point in the script)
+
+#   Configure Lucidworks Fusion to work in `binary` mode.
+#     - TODO: config file tinkering
+
+#   Spin up Apache Superset and Lucidworks Fusion.
+#     - TODO: start Superset
+#superset runserver -d
+FLASK_ENV=development flask run -p 8088 --with-threads --reload --debugger
