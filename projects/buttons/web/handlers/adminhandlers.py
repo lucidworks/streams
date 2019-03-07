87e84dc8ad6c151c61f6bd5a31f9925991d70173
Kord Campbell <kordless@gmail.com>
2019-03-06 16:31:48 -0800

    try logging the object before deleting it

--- projects/buttons/web/handlers/adminhandlers.py
+++ projects/buttons/web/handlers/adminhandlers.py
@@ -497,12 +497,12 @@ class AdminInstancesHandler(BaseHandler):
         # lookup user's auth info
         user_info = User.get_by_id(long(self.user_id))
 
+        slack.slack_message("Instance %s deleted for %s!" % (instance.name, user_info.username))
+
         # delete instance
         instance = Instance.get_by_id(long(instance_id))
         instance.key.delete()
         
-        slack.slack_message("Instance %s deleted for %s!" % (instance.name, user_info.username))
-
         # hangout for a second
         if config.isdev:
             time.sleep(1)
